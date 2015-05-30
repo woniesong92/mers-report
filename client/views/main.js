fbb40861d006cc8d9eba725757a2cf002a469f78
Howon Song <hs459@cornell.edu>
2015-05-30 13:00:43 -0700

    added infowindow form

--- client/views/main.js
+++ client/views/main.js
@@ -1,16 +1,37 @@
 if (Meteor.isClient) {
   // Session.setDefault('counter', 0);
 
+  // Handles the event where the user searches an address
   function geocodeHandler() {
     var address = $('.report-addr-input').val();
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
         map.setCenter(results[0].geometry.location);
         map.setZoom(15);
+
         var marker = new google.maps.Marker({
             map: map,
             position: results[0].geometry.location
         });
+
+        var html = "<div class='info-container'>" +
+                   "<form class='info-form'>" +
+                   "<div class='info-form-label'>신고 내용</div>" +
+                   "<textarea class='form-control' rows='5'></textarea>" +
+                   "<button class='btn btn-primary btn-block mers-info-btn'>등록</button>" +
+                   "</form>" +
+                   "</div>";
+
+        var infowindow = new google.maps.InfoWindow({
+          content: html
+        });
+        infowindow.open(map, marker);
+
+        $('.info-form').submit(function() {
+          debugger
+        })
+
+
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
