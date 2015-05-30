c16bee9f484e7252ed422c26e566cefd545dc07e
Howon Song <hs459@cornell.edu>
2015-05-30 15:30:46 -0700

    get rid of console msg

--- client/views/layout.js
+++ client/views/layout.js
@@ -3,7 +3,6 @@ if (Meteor.isClient) {
   Template.Layout.rendered = function () {
     var remainingHeight = $('body').height() - $('#content-block').height();
     var newHeight = remainingHeight / 2.0 + "px";
-    console.log(newHeight);
     $('#header, #footer').css('height', newHeight);
     $('#header, #footer').css('line-height', newHeight);
   }
